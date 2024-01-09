57b57bee6e0097ff06b7b3b8ac287e9efaa6a5db
Maygummy16 <128161999+Maygummy16@users.noreply.github.com>
2024-01-09 18:43:28 +0700

    Add files via upload

---
+++ SI.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+
+
+String one = "no";
+
+do {
+    Console.WriteLine("Enter principal : ");
+    double principal = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter Rate : ");
+    double Rate = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter Time : ");
+    double Time = Convert.ToDouble(Console.ReadLine());
+
+    double si = (principal*Rate*Time)/100;
+    Console.WriteLine(si);
+
+    Console.WriteLine("Calculate ST one more time ? : yes or no" );
+    one = Convert.ToString(Console.ReadLine());
+}
+while (one ==  "yes");

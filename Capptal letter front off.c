6421817562a8006674f25d9a97559f8f2d2cbbf5
Maygummy16 <128161999+Maygummy16@users.noreply.github.com>
2024-01-09 22:46:32 +0700

    Add files via upload

---
+++ Capptal letter front off.c
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+
+
+Console.WriteLine("Enter word");
+string word = Console.ReadLine();
+int i = 0;
+string newWord = "";
+while (i< word.Length){
+    if(i == 0){
+        newWord = newWord+char.ToUpper(word[i]);
+        i++;
+        continue;
+    }
+    if((word[i-1]) == ' '){
+    newWord = newWord+char.ToUpper(word[i]);
+    i++;
+    continue; 
+
+    }
+
+    newWord = newWord+word[i];
+    i++;
+}
+Console.WriteLine(newWord);

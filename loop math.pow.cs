51a4222f9be4421afddddeab6d74c74304ca1212
Maygummy16 <128161999+Maygummy16@users.noreply.github.com>
2024-01-10 20:50:14 +0700

    Add files via upload

---
+++ loop math.pow.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+using System.Xml.XPath;
+
+
+Console.WriteLine("Enter your power : " );
+int pw = Convert.ToInt32(Console.ReadLine());
+
+
+for( int i = 0 ; i <= pw; i++)
+{
+    Console.WriteLine("11**{0} = {1}: ",i,Math.Pow(11,i) );
+}
+
+
+

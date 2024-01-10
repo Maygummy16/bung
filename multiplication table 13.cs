074f0e2077c4afc43b014c3d9f183938f4c4682b
Maygummy16 <128161999+Maygummy16@users.noreply.github.com>
2024-01-10 17:34:37 +0700

    Add files via upload

---
+++ multiplication table 13.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+
+/*
+int x = 13;
+int i = 1;
+
+while(i <= 13){
+    int product = i*x;
+    Console.WriteLine("{0}*{1} = {2}" ,i,x,product);
+    i++;
+}
+*/
+
+/*
+int x = 13;
+int i = 1;
+do{
+    int product = i*x;
+    Console.WriteLine("{0}*{1} = {2}" ,i,x,product);
+    i++;
+
+}while(i <= 13);
+*/
+

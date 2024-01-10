e2c88c1579abadd7b4123091d2cc0724519d8286
Maygummy16 <128161999+Maygummy16@users.noreply.github.com>
2024-01-10 18:52:45 +0700

    Add files via upload

---
+++ num loob follow.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+
+/*
+Console.WriteLine("Enter your num : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int n = 1;
+while(n<=num){
+    Console.WriteLine(" result : {0}", n );
+    n++;
+
+}
+*/
+
+
+/*
+Console.WriteLine("Enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i =1 ; i <= num; i++){
+    Console.WriteLine(i);
+}
+*/
+
+

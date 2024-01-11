c5a0bd407602b4d25299b22d9deff030f13b528a
Maygummy16 <128161999+Maygummy16@users.noreply.github.com>
2024-01-11 12:11:18 +0700

    Add files via upload

---
+++ discount.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+using System.Xml.XPath;
+
+
+Console.WriteLine("Enter your price : ");
+double money = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your number : "); 
+double num = Convert.ToInt32(Console.ReadLine());
+
+double total = 0;
+for (int i = 0; i<=num; i++)
+{
+    if(i<=8)
+    {
+        total = total + money;
+    }
+    else
+    {
+        total = total + (money*0.8);
+    }
+}
+Console.WriteLine("total : {0}", total);

30cb80dd3d5db043eb8349bfb1f327a239f93bb4
Maygummy16 <128161999+Maygummy16@users.noreply.github.com>
2024-01-11 13:56:15 +0700

    Add files via upload

---
+++ %7.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter the last license range : " );
+int lr = Convert.ToInt32(Console.ReadLine());
+
+
+for(int i = 0; i <= lr; i++)
+{
+    if(i % 7==0)
+    {
+    Console.WriteLine("Bult {0}",i);
+    }
+}
+
+

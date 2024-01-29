f51764dd3ae9f6241ae4a6e30361ab3b2ece6772
Maygummy16 <128161999+Maygummy16@users.noreply.github.com>
2024-01-29 10:17:17 +0700

    Add files via upload

---
+++ Average class.cs
@@ -0,0 +1,19 @@
+﻿//Sumonrat Thana 
+//Average class
+
+using System.Linq;
+
+int[] A = {100,99,88,77,66};
+int[] B ={88,77,66,55,44};
+Console.WriteLine("Averange A : {0}", A.Average());
+Console.WriteLine("Averange B : {0}", B.Average());
+
+
+
+
+
+
+
+
+
+
---
+++ Average.cs
@@ -0,0 +1,18 @@
+﻿//Sumonrat Thana 
+//Average
+
+using System.Collections;
+using System.ComponentModel;
+using System.Runtime.Intrinsics.X86;
+
+double[] num = [42,50,45.5,48,21,19,29,36];
+
+
+Console.WriteLine("Max = {0}",num.Max());
+Console.WriteLine("Min = {0}",num.Min());
+Console.WriteLine("Av = {0}",num.Average());
+
+
+
+
+

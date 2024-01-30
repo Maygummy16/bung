fa28581e62b6d22a88e6c03fd25fa6e8d32094e0
Maygummy16 <128161999+Maygummy16@users.noreply.github.com>
2024-01-30 22:35:10 +0700

    Add files via upload

---
+++ pointer1.cs
@@ -0,0 +1,52 @@
+﻿//Sumonrat Thana 
+
+/*
+using System.Globalization;
+using System.Runtime.InteropServices;
+Console.WriteLine("Enter your num ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= num; i++){
+  
+    for(int j = 1; j<=i; j++){
+        Console.Write(" *"); //เเนวนอน
+    }
+    Console.WriteLine(); //เเนวตั้ง
+
+}
+for(int i = num; i >= num-1; i--){  
+  
+    for(int j = num; j>=i; j--){ 
+        Console.Write(" *");
+    }
+    Console.WriteLine();
+}
+
+/*
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+*/
+
+// Sumonrat Thana
+using System;
+
+
+class pointer1
+{
+    unsafe static void Main(string[] args)
+{
+    int num = 13;
+    int* ptnum = &num;
+    string strptnum = Convert.ToString((long)ptnum,16);
+    Console.WriteLine(strptnum);
+}
+}
+
+

741fa10583c119449b6dbe6d3f13d3cf6c820a5d
Maygummy16 <128161999+Maygummy16@users.noreply.github.com>
2024-01-30 22:38:04 +0700

    pointer1.cs

--- pointer1.cs
+++ pointer1.cs
@@ -1,38 +1,3 @@
-﻿//Sumonrat Thana 
-
-/*
-using System.Globalization;
-using System.Runtime.InteropServices;
-Console.WriteLine("Enter your num ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-for(int i = 1; i <= num; i++){
-  
-    for(int j = 1; j<=i; j++){
-        Console.Write(" *"); //เเนวนอน
-    }
-    Console.WriteLine(); //เเนวตั้ง
-
-}
-for(int i = num; i >= num-1; i--){  
-  
-    for(int j = num; j>=i; j--){ 
-        Console.Write(" *");
-    }
-    Console.WriteLine();
-}
-
-/*
-*
-**
-***
-****
-*****
-****
-***
-**
-*
-*/
 
 // Sumonrat Thana
 using System;

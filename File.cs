7ca8b03c8ab38f9dea68948b3451cbbce7d30bc3
Maygummy16 <128161999+Maygummy16@users.noreply.github.com>
2024-02-09 22:08:21 +0700

    Add files via upload

---
+++ File.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+// Sumonrat Thana
+using System;
+using System.IO;
+
+namespace Filehello
+{
+    class File
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter file = new StreamWriter("Sumonrat_Thana.txt");
+            Console.WriteLine("Enter your text :");
+            string str = Console.ReadLine();
+            file.WriteLine(str);
+            file.Flush();
+            file.Close();
+
+            StreamReader file1 = new StreamReader("Sumonrat_Thana.txt");
+            Console.WriteLine(file1.CurrentEncoding.ToString());
+            string temp = "";
+            while ((temp = file1.ReadLine()) != null) {
+            Console.WriteLine(temp);
+            }
+
+
+        
+        }
+    }
+
+
+}
